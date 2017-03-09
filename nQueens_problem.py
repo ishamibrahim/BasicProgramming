a2fe8114dbdcacfbca62dd17cb031ec1f56809e1
Isham Ibrahim <eyash.27@gmail.com>
2017-03-09 22:39:17 +0530

    Getting there

--- nQueens_problem.py
+++ nQueens_problem.py
@@ -6,23 +6,23 @@ CHESSBOARD = [  [0,1,0,0],
 
 
 
-def is_queen_safe(row, col):
-    for i in range(len(CHESSBOARD)):
-        if CHESSBOARD[row][i] or CHESSBOARD[i][col]:
+def is_queen_safe(row, col, chessboard):
+    for i in range(len(chess_board)):
+        if chessboard[row][i] or chessboard[i][col]:
             return False
     
     i = row
     j = col
     while (i>=0 and j>=0):
-        if CHESSBOARD[i][j]:
+        if chessboard[i][j]:
             return False
         i -= 1
         j -= 1
     
     i = row
     j = col
-    while (i<=0 and j > len(CHESSBOARD)):
-        if CHESSBOARD[i][j]:
+    while (i<=0 and j > len(chessboard)):
+        if chessboard[i][j]:
             return False
         i -= 1
         j += 1
@@ -30,4 +30,19 @@ def is_queen_safe(row, col):
 
 
 
-print is_queen_safe(3, 3)
+print is_queen_safe(3, 3, CHESSBOARD)
+
+def create_chessboard(size):
+    return [[0 for j in range(size)] for i in range(size)]
+
+def create nqueens(size):
+    safe_list = []
+    chessboard = create_chessboard(size)
+    chess_len = len(chessboard)
+    while True:
+        
+
+        
+    
+    
+
