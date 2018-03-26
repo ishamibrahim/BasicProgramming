d5b413bf0272736dd440c070c7ecf171dce000aa
ishamibrahim <Isham.Ibrahim@rackspace.com>
2018-03-26 13:19:03 +0530

    Binary tree creation using node objects

---
+++ treesNheaps.py
@@ -0,0 +1,48 @@
+import pdb
+
+#=====================BINARY TREE=======================================
+arr = [3, 97, 63, 55, 32, 56, 99, 22]
+
+
+class Bnode:
+    def __init__(self, val=None):
+        self.value = val
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return "value : {}".format(self.value)
+
+
+def create_binary_tree(linear_array):
+    root = None
+    curr_node = None
+    for item in linear_array:
+        node_item = Bnode(item)
+        if not root:
+            root = node_item
+        while True:
+            if not curr_node:
+                curr_node = node_item
+                break
+            elif not curr_node.value:
+                curr_node = node_item
+                break
+            elif item < curr_node.value:
+                if not curr_node.left:
+                    curr_node.left = node_item
+                    break
+                else:
+                    curr_node = curr_node.left
+
+            else:
+                if not curr_node.right:
+                    curr_node.right = node_item
+                    break
+                else:
+                    curr_node = curr_node.right
+        curr_node = root
+    return root
+
+
+tree = create_binary_tree(arr)
