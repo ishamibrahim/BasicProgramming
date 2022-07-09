37e386dcb3cd7376d1c91b99761fb24f5d36e7e6
ishamibrahim <eyash.27@gmail.com>
2022-07-09 23:43:34 +0530

    words from matrix

---
+++ DynamicProgramming/words_from_matrix.py
@@ -0,0 +1,69 @@
+import pdb
+
+"""
+    This is to list all possible words from a matrix that can occur by moving all eightways from word to word
+    in the matrix
+    
+    we check if a word exists in a matrix.. like a crossword puzzle except here the words need not be in a straight line
+"""
+# Below lists detail all eight possible movements from a cell
+# (top, right, bottom, left, and four diagonal moves)
+row = [-1, -1, -1, 0, 1, 0, 1, 1]
+col = [-1, 1, 0, -1, -1, 1, 0, 1]
+
+final_words = []
+
+
+def print_marked_locations(marked_locations):
+    final_seq = ""
+    for r in marked_locations:
+        final_seq += " ".join(["1" if i else "0" for i in r])
+        final_seq += "\n"
+    final_seq += "\n"
+    print(final_seq)
+
+
+def is_word_in_list(word, word_list):
+    return any(list_item.startswith(word) for list_item in word_list)
+
+
+# A recursive function to generate all possible words in a boggle
+def search_path_on_board(marked_locations, board, words, i, j, path=''):
+    path += board[i][j]
+    if is_word_in_list(path, words):
+        marked_locations[i][j] = True
+        if path in words:
+            print_marked_locations(marked_locations)
+            final_words.append(path)
+        for k in range(len(row)):
+            new_r = i + row[k]
+            new_c = j + col[k]
+
+            if 0 <= new_r < len(board) and 0 <= new_c < len(board[0]):
+                if not marked_locations[new_r][new_c]:
+                    search_path_on_board(marked_locations, board, words, new_r, new_c, path)
+
+    marked_locations[i][j] = False
+
+
+def search_words_in_board(words, board):
+    result = set()
+    marked_locations = [[False] * len(board[0]) for i in range(len(board))]
+    print(marked_locations)
+    path = ''
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            search_path_on_board(marked_locations, board, words, i, j)
+
+
+if __name__ == '__main__':
+    board = [
+        ['M', 'S', 'E'],
+        ['R', 'A', 'T'],
+        ['L', 'O', 'N']
+    ]
+
+    words = ['STAR', 'NOTE', 'SAND', 'STONE', 'STAN']
+
+    search_words_in_board(words, board)
+    print(final_words)
