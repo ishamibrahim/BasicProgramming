c63f3d436f2cefed34fa220a2a504a19b21c66e5
ishamibrahim <eyash.27@gmail.com>
2022-07-12 13:55:37 +0530

    BST and sliding windows problem

---
+++ sliding_windows.py
@@ -0,0 +1,97 @@
+"""
+    Problem 1. Given an array, find all contiguous sub-arrays that add up to a given number
+"""
+from typing import List
+
+given_arr = [1, 7, 4, 3, 1, 2, 1, 5, 1]
+
+
+def find_sum(givenarr : list, start: int, end: int):
+    return sum(givenarr[start:end])
+
+
+def find_indices_of_sum(givenarr: list, sum_to_find: int) -> set:
+
+    start = 0
+    end = 1
+    indices_that_total = set()
+    while end <= len(givenarr):
+        sum_found = find_sum(givenarr, start, end)
+        if sum_found == sum_to_find:
+            indices_that_total.add((start, end))
+            end += 1
+        elif sum_found < sum_to_find:
+            end += 1
+        else:
+            start += 1
+    return indices_that_total
+
+
+"""
+    Problem 2. Given test string, find all sub_strings whose characters are in a given second string
+"""
+input_str = "fa4chba4c444cba"
+match_str = "abc"
+
+
+def is_chars_in_string(char_str, test_sub_str):
+    is_exists = True
+    for single_char in char_str:
+        if single_char not in test_sub_str:
+            is_exists = False
+            break
+    return is_exists
+
+
+def find_all_chars_in_string(test_str : str, char_str: str) -> List[str]:
+    start = 0
+    end = 1
+    substring_list = list()
+    while start < len(test_str) and end <= len(test_str):
+        sub_str = test_str[start:end]
+        print(sub_str)
+        if is_chars_in_string(char_str, sub_str):
+            substring_list.append(sub_str)
+            start += 1
+        else:
+            if end > len(test_str):
+                start += 1
+            else:
+                end += 1
+    return substring_list
+
+
+"""
+    Problem 2. Given test string, find shortedst sub_strings whose characters are in a given second string
+    For this we copy the previous problem
+"""
+
+
+def assign_shortest(shorter_srt: str, shortest_str: str) -> str:
+
+    return shorter_srt if len(shorter_srt) < len(shortest_str) else shortest_str
+
+
+def find_shortest_substring_with_chars(test_str : str, char_str: str):
+    start = 0
+    end = 1
+    shortest_string = test_str
+    while start < len(test_str) and end <= len(test_str):
+        sub_str = test_str[start:end]
+
+        if is_chars_in_string(char_str, sub_str):
+            shortest_string = assign_shortest(sub_str, shortest_string)
+            print(shortest_string)
+            start += 1
+        else:
+            if end > len(test_str):
+                start += 1
+            else:
+                end += 1
+    print(shortest_string)
+
+
+if __name__ == '__main__':
+    # print(find_indices_of_sum(given_arr, 7))
+    # find_all_chars_in_string(input_str, match_str)
+    find_shortest_substring_with_chars(input_str, match_str)
--- trees_n_heaps/is_binary_search_tree.py
+++ trees_n_heaps/is_binary_search_tree.py
@@ -53,7 +53,7 @@ def create_binary_search_tree(linear_array):
 """
 Given a binary tree in the form of an array, check if its a binary search tree or not
 """
-arr = [0, 10, 7, 12, 4, 9, 0, 0, 1, 5, 0, 0, 0, 0, 0, 0, ]
+new_arr = [0, 10, 7, 12, 4, 9, 0, 0, 1, 5, 0, 0, 0, 0, 0, 0 ]
 
 
 class Node:
@@ -62,23 +62,31 @@ class Node:
         self.left = None
         self.right = None
 
+    @staticmethod
+    def print_tree(node):
+        if node == None:
+            return
+        Node.print_tree(node.left)
+        print(node.data)
+        Node.print_tree(node.right)
+
 
 last_parsed = -1
 
 
-def input_items(node, i):
-    if arr[i] != 0:
-        node.data = arr[i]
+def input_items(node, i, node_arr):
+    if node_arr[i] != 0:
+        node.data = node_arr[i]
     l = i * 2
-    if l < len(arr):
-        if arr[l] != 0:
+    if l < len(node_arr):
+        if node_arr[l] != 0:
             node.left = Node()
-            input_items(node.left, l)
+            input_items(node.left, l, node_arr)
     r = (i * 2) + 1
-    if l < len(arr):
-        if arr[r] != 0:
+    if l < len(node_arr):
+        if node_arr[r] != 0:
             node.right = Node()
-            input_items(node.right, r)
+            input_items(node.right, r, node_arr)
 
 
 def check_binary_search_tree(root):
@@ -92,12 +100,11 @@ def check_for_bst(node, result):
     if node.left:
         result = check_for_bst(node.left, result)
     if node.data <= last_parsed:
-        print("LAst parsed lesser")
+        print("Last parsed lesser")
         result = "No"
 
-    print("{0}  {1}".format(last_parsed, node.data))
+    print("{0} --> {1}".format(last_parsed, node.data))
     last_parsed = node.data
-
     if node.right:
         result = check_for_bst(node.right, result)
     return result
@@ -105,5 +112,6 @@ def check_for_bst(node, result):
 
 c = Node()
 
-input_items(c, 1)
+input_items(c, 1, new_arr)
+Node.print_tree(c)
 print(check_binary_search_tree(c))
