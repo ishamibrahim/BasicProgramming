a768afdf227b8ad8dab757d7892d3fc707ac054d
isham <eyash.27@gmail.com>
2023-06-14 17:24:37 +0530

    LC  problems II

--- LeetCode/lc_136_find_single_number.py
+++ LeetCode/lc_136_find_single_number.py
@@ -1,7 +1,7 @@
 from typing import List
 
 """
-https://leetcode.com/problems/single-number/submissions/971002828/
+https://leetcode.com/problems/single-number
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 Solution:
