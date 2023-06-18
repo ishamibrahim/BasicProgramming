b6ea09be4eac715c663eaa96f226c2d17904a8a7
isham <eyash.27@gmail.com>
2023-06-18 23:39:24 +0530

    LC  problems V

---
+++ Competitive/lru_cache_using_fixed_number_of_items.py
@@ -0,0 +1,83 @@
+
+ITEMS_IN_CACHE = 4
+
+
+# A doubly linked list will keep the numbered items
+class DoubleNode:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.item_count = 2
+        self.cache = dict()
+        self.left, self.right = DoubleNode(0, 0), DoubleNode(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def __repr__(self):
+        this = self.left
+        result = ""
+        while this:
+            result += "{}->".format(this.val)
+            this = this.next
+        return result
+
+    def _remove_node(self, node):
+
+        if node.key in self.cache:
+            del(self.cache[node.key])
+        prev, nex = node.prev, node.next
+        if prev:
+            prev.next = nex
+        if nex:
+            nex.prev = prev
+        node.next, node.prev = None, None
+        self.item_count -= 1
+
+    def _insert_node(self, node):
+        self.cache[node.key] = node
+        node.next = self.left
+        self.left.prev = node
+        self.left = node
+        self.item_count += 1
+
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            if node == self.right:
+                    self.right = node.prev
+            self._remove_node(node)
+            self._insert_node(node)
+            return node.val
+        return -1
+
+    def put(self, key, val):
+        if self.item_count == self.capacity:
+            right_node = self.right
+            self.right = right_node.prev
+            self._remove_node(right_node)
+        new_node = DoubleNode(key, val)
+        self._insert_node(new_node)
+
+
+if __name__ == "__main__":
+    lru_cache = LRUCache(ITEMS_IN_CACHE)
+    lru_cache.put(1, "a")
+    lru_cache.put(2, "b")
+    print(lru_cache.get(1))
+    lru_cache.put(3,"c")
+    lru_cache.put(4, "d")
+    print(lru_cache.get(2))
+    lru_cache.put(5, "e")
+    a = input()
+
+
+
+
+
+
+
---
+++ LeetCode/lc234_palindrome_linked_list.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from typing import Optional
+
+from LeetCode.constants import ListNode
+"""
+--easy--
+https://leetcode.com/problems/palindrome-linked-list/
+
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+This solution is from neetcode video. The solution I thought of was storing values in an array and iterating from both ends
+
+"""
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        """
+        Solution -  Runtime: 92%
+                    Memory: 75.6%
+        """
+
+        result = True
+        front = back = head
+        # using hare and tortoise method we reach the middle of the linkedlist
+        while front and front.next:
+            front = front.next.next
+            back = back.next
+        mid = back
+
+        # Reverse the linkedlist from the middle iteratively (cal also use recursion)
+        temp = mid
+        nex = mid.next
+        temp.next = None
+
+        while nex:
+            mid = nex
+            nex = mid.next
+            mid.next = temp
+            temp = mid
+
+        # Check if two linked lists are equal
+        while mid:
+            if mid.val != head.val:
+                result = False
+                break
+            mid, head = mid.next, head.next
+
+        return result
+
+
+
+
+
+ll = ListNode(6, ListNode(6, ListNode(90, ListNode(45, ListNode(55, ListNode(21))))))
+l1 = ListNode(6, ListNode(5, ListNode(5, ListNode(6))))
+l2 = ListNode(3, ListNode(4))
+s_time = datetime.now()
+print(Solution().isPalindrome(l2))
+print("The solution took %s "%(datetime.now()-s_time))
+
+
